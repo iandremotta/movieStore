99e27984486769f78617ea26f004d3277417c781
Andre Motta <iandremotta@gmail.com>
2022-06-24 14:21:55 -0300

    first commit

---
+++ MovieStore/Data/MovieStoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieStore.Models;
+
+namespace MovieStore.Data
+{
+    public class MovieStoreContext : DbContext
+    {
+        public MovieStoreContext (DbContextOptions<MovieStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
